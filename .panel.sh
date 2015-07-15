a431167ec10b7ad82da79065e2c60ef12c81a97c
Mateusz Łukasik <mati75@linuxmint.pl>
2015-07-15 13:40:30 +0200

    update conky config for 1.10

--- .panel.sh
+++ .panel.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export LC_ALL=en_GB.UTF-8
 HEIGHT=`xdpyinfo | grep dimensions | awk '{print $2}' | awk -Fx '{print $1}'`
+#FONT='-*-terminus-*-*-*-*-*-*-*-*-*-*-*-*'
 FONT="-gohu-gohufont-medium-r-normal--11-80-100-100-c-60-iso10646-1"
-exec conky -d -c "/home/mati75/.conky" | dzen2 -fg "#C7C79D" -bg "#151515" -ta left -w $HEIGHT -h 11 -x 0 -y 0 -fn $FONT &
+exec conky -d -c "/home/mati75/.conky" | dzen2 -p -fg "#C7C79D" -bg "#151515" -ta left -w $HEIGHT -h 11 -x 0 -y 0 -fn $FONT &
 exit 0
 
